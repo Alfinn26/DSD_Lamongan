faabd499e35739a91ac75e14940e7baf9e89848c
Alfinn26 <alfinsabil647@gmail.com>
2026-05-01 07:22:28 -0700

    Refactor file upload event listener and image preview

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const upload = document.getElementById("upload");
 const preview = document.getElementById("preview");
 
-upload.addEventListener("change", function(){
+upload.addEventListener("change", () => {
     preview.innerHTML = "";
 
-    Array.from(upload.files).forEach(file => {
+    [...upload.files].forEach(file => {
         const reader = new FileReader();
 
-        reader.onload = function(e){
+        reader.onload = e => {
             const img = document.createElement("img");
             img.src = e.target.result;
             preview.appendChild(img);
-        }
+        };
 
         reader.readAsDataURL(file);
     });
